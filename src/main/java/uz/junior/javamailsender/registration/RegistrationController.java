f210d5e4a4162b1bd918817367d186aa06f5b8e1
Ramz <s_ramis@inbox.ru>
2022-01-13 20:04:23 +0500

    checked registration POSTMAN

--- src/main/java/uz/junior/javamailsender/registration/RegistrationController.java
+++ src/main/java/uz/junior/javamailsender/registration/RegistrationController.java
@@ -2,6 +2,7 @@ package uz.junior.javamailsender.registration;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,8 @@ public class RegistrationController {
 
     private RegistrationService registrationService;
 
-    public String regiter(@RequestBody RegistrationRequest request) {
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
 
