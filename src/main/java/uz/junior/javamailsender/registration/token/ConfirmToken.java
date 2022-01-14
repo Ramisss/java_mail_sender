b95d3c5017aff3ef2b18b9bd42b03639362ca7e8
Ramz <s_ramis@inbox.ru>
2022-01-14 15:49:06 +0500

    Confirmation TOKEN @Entity

--- src/main/java/uz/junior/javamailsender/registration/token/ConfirmToken.java
+++ src/main/java/uz/junior/javamailsender/registration/token/ConfirmToken.java
@@ -1,37 +1,50 @@
-//package uz.junior.javamailsender.registration.token;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//
-//import javax.persistence.*;
-//import java.time.LocalDateTime;
-//
-//@AllArgsConstructor
-//@Data
-//@Entity
-//public class ConfirmToken {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Long id;
-//
-//    @Column(nullable = false)
-//    private String token;
-//
-//    private LocalDateTime createdAt;
-//    private LocalDateTime expiredAt;
-//    private LocalDateTime confirmedAt;
-//
-//    public ConfirmToken(String token,
-//                        LocalDateTime createdAt,
-//                        LocalDateTime expiredAt,
-//                        LocalDateTime confirmedAt) {
-//        this.token = token;
-//        this.createdAt = createdAt;
-//        this.expiredAt = expiredAt;
-//        this.confirmedAt = confirmedAt;
-//    }
-//
-//    public ConfirmToken() {
-//
-//    }
-//}
+package uz.junior.javamailsender.registration.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import uz.junior.javamailsender.appuser.AppUser;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Data
+@Entity
+public class ConfirmToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiredAt;
+
+    @Column(nullable = false)
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(
+            nullable = false,
+            name = "app_user_id"
+    )
+    private AppUser appUser;
+
+    public ConfirmToken(String token,
+                        LocalDateTime createdAt,
+                        LocalDateTime expiredAt,
+                        LocalDateTime confirmedAt) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiredAt = expiredAt;
+        this.confirmedAt = confirmedAt;
+    }
+
+    public ConfirmToken() {
+
+    }
+}
